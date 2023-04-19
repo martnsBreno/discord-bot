9908b2989c638888e56d82b0aaed15c77474dccd
Breno Martins <102875429+martnsBreno@users.noreply.github.com>
2023-04-19 08:45:08 -0300

    Now she plays music xD

--- src/main/java/com/github/martns/App.java
+++ src/main/java/com/github/martns/App.java
@@ -1,15 +1,28 @@
 package com.github.martns;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.security.auth.login.LoginException;
 
 import com.github.martns.interfaces.Command;
+import com.github.martns.util.LavaPlayerAudioProvider;
+import com.github.martns.util.TrackScheduler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import discord4j.voice.AudioProvider;
 import io.github.cdimascio.dotenv.Dotenv;
 
 /**
@@ -26,6 +39,45 @@ public class App {
 
         String token = env.get("DISCORD_TOKEN");
 
+        // Creates AudioPlayer instances and translates URLs to AudioTrack instances
+        final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+
+        // This is an optimization strategy that Discord4J can utilize.
+        // It is not important to understand
+        playerManager.getConfiguration()
+                .setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+
+        // Allow playerManager to parse remote sources like YouTube links
+        AudioSourceManagers.registerRemoteSources(playerManager);
+
+        // Create an AudioPlayer so Discord4J can receive audio data
+        final AudioPlayer player = playerManager.createPlayer();
+
+        // We will be creating LavaPlayerAudioProvider in the next step
+        AudioProvider provider = new LavaPlayerAudioProvider(player);
+
+        commands.put("join", event -> {
+            final Member member = event.getMember().orElse(null);
+            if (member != null) {
+                final VoiceState voiceState = member.getVoiceState().block();
+                if (voiceState != null) {
+                    final VoiceChannel channel = voiceState.getChannel().block();
+                    if (channel != null) {
+                        // join returns a VoiceConnection which would be required if we were
+                        // adding disconnection features, but for now we are just ignoring it.
+                        channel.join(spec -> spec.setProvider(provider)).block();
+                    }
+                }
+            }
+        });
+
+        final TrackScheduler scheduler = new TrackScheduler(player);
+        commands.put("play", event -> {
+            final String content = event.getMessage().getContent();
+            final List<String> command = Arrays.asList(content.split(" "));
+            playerManager.loadItem(command.get(1), scheduler);
+        });
+
         final GatewayDiscordClient client = DiscordClientBuilder.create(token).build()
                 .login()
                 .block();
@@ -39,7 +91,7 @@ public class App {
 
                     for (final Map.Entry<String, Command> entry : commands.entrySet()) {
                         // We will be using ! as our "prefix" to any command in the system.
-                        if (content.startsWith('%' + entry.getKey())) {
+                        if (content.startsWith('!' + entry.getKey())) {
                             entry.getValue().execute(event);
                             break;
                         }
